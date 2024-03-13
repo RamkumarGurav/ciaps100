54e1c07d984e0db05a14fa1c06745bb6adffa370
ramkumar gurav <ramkumarsgurav@gmail.com>
2024-03-13 19:52:34 +0530

    2nd commit

--- application/config/routes.php
+++ application/config/routes.php
@@ -49,6 +49,16 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | Examples:	my-controller/index	-> my_controller/index
 |		my-controller/my-method	-> my_controller/my_method
 */
-$route['default_controller'] = 'welcome';
+$route['default_controller'] = 'auth';
 $route['404_override'] = '';
-$route['translate_uri_dashes'] = FALSE;
+$route['translate_uri_dashes'] = TRUE;
+
+
+$route['login'] = 'auth/index';
+
+
+$route['dashboard/financial_years'] = 'financialyears/index';
+$route['dashboard/albums'] = 'album/index';
+$route['dashboard/gallery'] = 'gallery/index';
+
+$route['dashboard'] = 'dashboard/index';
--- application/controllers/Auth.php
+++ application/controllers/Auth.php
@@ -1,15 +1,16 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 class Auth extends CI_Controller {
-public function index()
-{
-$this->load->view('welcome_message');
-redirect('dashboard');
+
+public function index(){
+  $this->load->library('validation');
+  
+
+
+$this->load->view("auth/login");
 }
 
 public function login(){
 
-
-  redirect('dashboard');
 }
 }
